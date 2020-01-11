5217f8729f8b3fac55c6a907512694fe32cd79dc
Konerinstintin <38484296+Konerinstintin@users.noreply.github.com>
2020-01-11 14:53:15 -0500

    Updated Files

--- Trivia.js
+++ Trivia.js
@@ -1,36 +1,35 @@
 function clickCounter() {
-	if (typeof (Storage) !== "undefined") {
-		if (localStorage.clickcount) {
-			localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
 
-			if (localStorage.clickcount < 10) {
-				document.getElementById("score").innerHTML = (((localStorage.clickcount / 13) * 100).toFixed(0)) + "%";
-				document.getElementById("result").innerHTML = ("Sorry but you have scored less than the required amount to unlock the online quiz please click &#34;Reset&#34; and try agian");
-			} else if (localStorage.clickcount <= 13) {
-				document.getElementById("score").innerHTML = (((localStorage.clickcount / 13) * 100).toFixed(0)) + "%";
-				document.getElementById("result").innerHTML = ("Congradulations you have scored more than 80% on the quiz, you can now continue to the next online quiz<br><br>https://www.google.ca");
-			}
-		} else {
-			localStorage.clickcount = 1;
-		}
-	} else {
-		localStorage.clickcount = 1;
-	}
+        } else {
+            localStorage.clickcount = 0;
+        }
+    } else {
+        localStorage.clickcount = 0;
+    }
 }
 
-function result() {
-	(localStorage.clickcount)
-	localStorage.clickcount = Number(localStorage.clickcount) + 1;
 
-	if (localStorage.clickcount < 10) {
-		document.getElementById("score").innerHTML = (((localStorage.clickcount / 13) * 100).toFixed(0)) + "%";
-		document.getElementById("result").innerHTML = ("Sorry but you have scored less than the required amount to unlock the online quiz please click &#34;Reset&#34; and try agian");
-	} else if (localStorage.clickcount <= 13) {
-		document.getElementById("score").innerHTML = (((localStorage.clickcount / 13) * 100).toFixed(0)) + "%";
-		document.getElementById("result").innerHTML = ("Congradulations you have scored more than 80% on the quiz, you can now continue to the next online quiz<br><br>https://www.google.ca");
-	}
+
+function result() {
+    document.getElementById("score").innerHTML = (((localStorage.clickcount + 1) / 12) * 100).toFixed(0) + "%";
+    var desision = (((localStorage.clickcount + 1) / 12) * 100).toFixed(0);
+
+    if (desision < 80 || desision == 0) {
+
+        document.getElementById("result").innerHTML = ("Sorry but you have scored less than the required amount to unlock the online quiz please click &#34;Reset&#34; and try agian");
+    } else if (desision <= 100) {
+
+        document.getElementById("result").innerHTML = ("Congradulations you have scored more than 80% on the quiz, you can now continue to the next online quiz<br><br>https://www.google.ca");
+    } else if (localStorage.clickcount == 0) {
+        dsocument.getElementById("result").innerHTML = ("Sorry but you have scored less than the required amount to unlock the online quiz please click &#34;Reset&#34; and try agian");
+    } else {
+        document.write("Error press reset");
+    }
 }
 
 function deleteItems() {
-	localStorage.clear();
+    localStorage.clear();
 }
