d371ace8b2770c3fa07c0627230bce322b2975c8
Konerinstintin <38484296+Konerinstintin@users.noreply.github.com>
2020-01-11 15:07:55 -0500

    Update Trivia.js

--- Trivia.js
+++ Trivia.js
@@ -4,29 +4,27 @@ function clickCounter() {
             localStorage.clickcount = Number(localStorage.clickcount) + 1;
 
         } else {
-            localStorage.clickcount = 0;
+            localStorage.clickcount = 1;
         }
     } else {
-        localStorage.clickcount = 0;
+        localStorage.clickcount = 1;
     }
 }
 
 
 
 function result() {
-    document.getElementById("score").innerHTML = (((localStorage.clickcount + 1) / 12) * 100).toFixed(0) + "%";
-    var desision = (((localStorage.clickcount + 1) / 12) * 100).toFixed(0);
+    document.getElementById("score").innerHTML = (((localStorage.clickcount / 12) * 100).toFixed(0)) + "%";
+    var desision = (((localStorage.clickcount / 12) * 100).toFixed(0));
 
-    if (desision < 80 || desision == 0) {
+    if (desision < 80) {
 
         document.getElementById("result").innerHTML = ("Sorry but you have scored less than the required amount to unlock the online quiz please click &#34;Reset&#34; and try agian");
     } else if (desision <= 100) {
 
         document.getElementById("result").innerHTML = ("Congradulations you have scored more than 80% on the quiz, you can now continue to the next online quiz<br><br>https://www.google.ca");
-    } else if (localStorage.clickcount == 0) {
-        dsocument.getElementById("result").innerHTML = ("Sorry but you have scored less than the required amount to unlock the online quiz please click &#34;Reset&#34; and try agian");
     } else {
-        document.write("Error press reset");
+        document.getElementById("result").innerHTML = ("You did not get anything right please press reset");
     }
 }
 
